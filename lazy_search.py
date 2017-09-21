a24edaadb26d578de6c27ad1936428b9185bf684
Massih <massih.z@gmail.com>
2017-09-21 23:29:43 +0200

    separating stackoverflow

--- lazy_search.py
+++ lazy_search.py
@@ -3,45 +3,47 @@ from __future__ import unicode_literals
 
 import json
 import requests
-from pprint import pprint
 from prompt_toolkit import prompt
 
 STACKOVERFLOW_URL = 'https://api.stackexchange.com/2.2/search/advanced'
 
-def ask_for(search_prompt):
-    return prompt(search_prompt)
 
-def print_stuff(data, extra=False):
-    print('='*20)
+def ask_user(prompt_msg):
+    prompt(prompt_msg)
+
+
+def print_stackoverflow_result(data, extra=False):
+    print('=' * 20)
     print('Remaining: {}'.format(data['quota_remaining']))
     print('Items:')
     for item in data['items']:
         print('=========')
         print(item)
         print('=========')
-#     print('\n'.join(data['items']))
     if extra:
         print('Extra:')
         print('Has more: {}'.format(data['has_more']))
 
-def api_call(search_text, payload=None):
-    payload = payload or {
+
+def stackoverflow_call(query):
+    payload = {
         'order': 'desc',
-        'sort':'activity',
+        'sort': 'activity',
         'site': 'stackoverflow',
-        'q': '',
+        'q': query,
         'accepted': True,
         'body': '',
         'nottagged': '',
         'tagged': '',
-        'title': search_text
+        'title': ''
     }
-    response = requests.get(STACKOVERFLOW_URL, params=payload)
-    print_stuff(json.loads(response.text))
+    return json.loads(requests.get(STACKOVERFLOW_URL, params=payload).text)
+
 
 def main():
-    search_text = ask_for('Search: ')
-    api_call(search_text)
+    query = ask_user('Search:')
+    response = stackoverflow_call(query)
+    print_stackoverflow_result(response)
 
 
 if __name__ == '__main__':
